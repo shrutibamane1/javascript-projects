8b6d91cfb185c4d2d95eb23132261d8893582e40
shrutibamane1 <shrutibamane444@gmail.com>
2026-05-18 13:02:18 +0000

    consversion of data type

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,30 @@
-console.log("shruti");
+let score = "22mm"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+
+
+// "33" =>33
+// "33uul" =>NaN
+//true =>1' flase =>0
+
+// let islog =""
+// let boolean = Boolean(islog)
+// console.log(boolean);
+
+// 1 => true ; 0=> false
+// "" => false
+//"shruti" =>true
+
+let str =33
+
+let num = String(str)
+console.log(num);
+console.log(typeof num);
+
+
