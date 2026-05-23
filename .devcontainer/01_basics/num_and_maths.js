5e624f44466337416ac70180aaa4ccb092132e7f
shrutibamane1 <shrutibamane444@gmail.com>
2026-05-23 19:19:19 +0530

    Update num_and_maths.js

--- .devcontainer/01_basics/num_and_maths.js
+++ .devcontainer/01_basics/num_and_maths.js
@@ -1,1 +1,48 @@
+const score = 100
+// console.log(score);   
+
+// const balance = new Number(400)
+// console.log(balance);         //[Number: 400]
+
+// console.log(balance.toString());  //400
+// console.log(balance.toFixed(2));  //400.00
+// console.log(typeof(balance));    //object
+
+// const num = 12.50
+// console.log(num.toPrecision(3));  //12.5
+
+// const num2 = 333200000
+// console.log(num2.toLocaleString('en-IN'));   // 33,32,00,000
+
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-7));    //7   convert only negative value to positive ....but not in positive in nagative!!
+// console.log(Math.round(4.6));  //5
+// console.log(Math.ceil(4.1));  //5
+// console.log(Math.floor(4.1));  //4
+// console.log(Math.min(1,3,5,0)); //0
+// console.log(Math.max(1,3,5,8));  //9
+
+console.log(Math.random());   //display value only inbetween 0-1 
+console.log((Math.random()*10) + 1);
+console.log(Math.ceil(Math.random()*10) + 1);
+
+
+const min = 10
+const max =20
+console.log(Math.floor(Math.random() * (max-min +1)) + min);   //10
+
+
+
+
+
+
+
+
+
+
+ 
+
 
